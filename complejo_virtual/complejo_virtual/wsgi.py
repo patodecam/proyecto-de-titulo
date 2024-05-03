de75c74570c45490b18e73db0ea849b8dbaed745
Patodecam <patodecam.1997@gmail.com>
2024-05-03 02:14:01 -0400

    Primer commit del proyecto base en django

---
+++ complejo_virtual/complejo_virtual/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for complejo_virtual project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'complejo_virtual.settings')
+
+application = get_wsgi_application()
