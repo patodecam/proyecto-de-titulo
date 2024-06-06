032225fc45ac4cbb95407a097b37205346260f0d
Sebastian <sebigonza1701@gmail.com>
2024-06-06 01:28:04 -0400

    Configuracion grafica del calendario

--- complejo_virtual/home/static/js/home.js
+++ complejo_virtual/home/static/js/home.js
@@ -1,22 +1,47 @@
-import { Calendar } from '@fullcalendar/core'
-import dayGridPlugin from '@fullcalendar/daygrid'
-import bootstrap5Plugin from '@fullcalendar/bootstrap5'
-
 document.addEventListener('DOMContentLoaded', function() {
   const calendarEl = document.getElementById('calendar')
-  const calendar = new Calendar(calendarEl, {
-    plugins: [
-      dayGridPlugin,
-      bootstrap5Plugin
-    ],
+  const calendar = new FullCalendar.Calendar(calendarEl, {
+    themeSystem: 'bootstrap5',
     headerToolbar: {
-      left: 'prev,next today',
+      left:'prevYear,prev,next,nextYear today',
       center: 'title',
-      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+      right: 'dayGridMonth,timeGridWeek,timeGridDay'
+    },
+    buttonText:{
+      today:    'Hoy',
+      month:    'Mes',
+      week:     'Semana',
+      day:      'Día',
+      list:     'Lista'
+    },
+    titleFormat: { year: 'numeric', month: 'long', day: 'numeric' },
+
+    navLinks: true, // can click day/week names to navigate views
+    editable: true,
+    selectable: true,
+    selectMirror: true,
+    
+    select: function(arg) {
+      var title = prompt('Event Title:');
+      if (title) {
+        calendar.addEvent({
+          title: title,
+          start: arg.start,
+          end: arg.end,
+          allDay: arg.allDay
+        })
+      }
+      calendar.unselect()
+    },
+    eventClick: function(arg) {
+      if (confirm('Are you sure you want to delete this event?')) {
+        arg.event.remove()
+      }
     },
     events: [
       { title: 'Meeting', start: new Date() }
     ]
   })
   calendar.render()
+  calendar.setOption('locale','es')
 })
